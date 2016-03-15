1e770da83a5ad92155d504c19ecf4f8049058db2
tacowhisperer <chato94@gmail.com>
2016-03-15 01:30:02 -0500

    Added more commands, and general cleanup of logs and code.

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,51 +1,88 @@
+var fs       = require ('fs'),
+	readline = require ('readline'),
+	PNG      = require ('pngjs').PNG,
+	colors   = require ('colors');
+
 // Determine argument options for including the HTML file and the names for the
 // PNG and HTML files
 var includeHTML = false,
 	fileName = false;
 
 // Mandelbrot default values
-var width = 5760,
-	height = 5280,
+var width = 1440,
+	height = 1320,
 	xmin = -2,
 	xmax = 0.55,
 	ycenter = 0,
 	maxMagnitude = 50,
-	iterations = 100;
+	iterations = 150,
 
+	includeR = 0x0,
+	includeG = 0x0,
+	includeB = 0x0,
+	includeA = 0xff,
+	escapeR  = 0xff,
+	escapeG  = 0xff,
+	escapeB  = 0xff,
+	escapeA  = 0xff;
+
+// Gracefully exit node by letting the user know that the image might be corrupted
+process.on ('SIGINT', function () {
+	console.log ('\nThe current process has been terminated. Note that the files might be corrupted.');
+	process.exit (0);
+});
+
+// Begin mandelbrot calculation loops
 for (var i = 2; i < process.argv.length; i++) {
 	if (process.argv[i].match (/^--?html$/i))
 		includeHTML = true;
 
-	else if (process.argv[i].match (/^--?filename\./i))
-		fileName = process.argv[i].replace (/\/|\?|<|>|\\|:|\*|\||"|^--?filename\./g, '');
+	else if (process.argv[i].match (/^--?filename:/i))
+		fileName = process.argv[i].replace (/\/|\?|<|>|\\|:|\*|\||"|^--?filename:/gi, '');
 
-	else if (process.argv[i].match (/^--?size\.\d+\D\d+$/i)) {
+	else if (process.argv[i].match (/^--?size:\d+\D\d+$/i)) {
 		var dimensions = process.argv[i].match (/\d+/g);
 		width = dimensions[0];
 		height = dimensions[1];
 	}
 
-	else if (process.argv[i].match (/^--?xmin\.\-?\d+\.?\d*$/i))
+	else if (process.argv[i].match (/^--?xmin:\-?\d+\.?\d*$/i))
 		xmin = +process.argv[i].match (/\-?\d+\.?\d*/)[0];
 
-	else if (process.argv[i].match (/^--?xmax\.\-?\d+\.?\d*$/i))
+	else if (process.argv[i].match (/^--?xmax:\-?\d+\.?\d*$/i))
 		xmax = +process.argv[i].match (/\-?\d+\.?\d*/)[0];
 
-	else if (process.argv[i].match (/^--?ycenter\.\-?\d+\.?\d*$/i))
+	else if (process.argv[i].match (/^--?ycenter:\-?\d+\.?\d*$/i))
 		ycenter = +process.argv[i].match (/\-?\d+\.?\d*/)[0];
 
-	else if (process.argv[i].match (/^--?maxMagnitude\.\-?\d+\.?\d*$/i))
+	else if (process.argv[i].match (/^--?maxMagnitude:\-?\d+\.?\d*$/i))
 		maxMagnitude = +process.argv[i].match (/\-?\d+\.?\d*/)[0];
 
-	else if (process.argv[i].match (/^--?iterations\.\d+$/i))
+	else if (process.argv[i].match (/^--?iterations:\d+$/i))
 		iterations = +process.argv[i].match (/\d+/)[0];
+
+	else if (process.argv[i].match (/^--?includeRGB:#[0-9a-f]{6}$/i)) {
+		var components = process.argv[i].replace (/^--?includeRGBA:#/i, '').match (/[0-9a-f]{2}/gi);
+		includeR = +('0x' + components[0]);
+		includeG = +('0x' + components[1]);
+		includeB = +('0x' + components[2]);
+		includeA = +('0x' + components[3]);
+	}
+
+	else if (process.argv[i].match (/^--?escapeRGB:#[0-9a-f]{6}$/i)) {
+		var components = process.argv[i].replace (/^--?escapeRGBA:#/i, '').match (/[0-9a-f]{2}/gi);
+		escapeR = +('0x' + components[0]);
+		escapeG = +('0x' + components[1]);
+		escapeB = +('0x' + components[2]);
+		escapeA = +('0x' + components[3]);
+	}
 }
 
-var fs = require ('fs'),
-	readline = require ('readline'),
-	PNG = require ('pngjs').PNG,
-	htmlFile = fileName? fileName + '.html' : 'mandelbrot.html',
-	pngFile = fileName? fileName + '.png' : 'mandelbrot.png';
+// Hot fix for negative y-center movement
+ycenter *= -1;
+
+var htmlFile = fileName? fileName + '.html' : 'mandelbrot.html',
+	pngFile  = fileName? fileName + '.png' : 'mandelbrot.png';
 
 // Used to map the ascii corresponding to a shade value range
 function calculatePixel (v) {
@@ -81,17 +118,27 @@ function calculatePixel (v) {
 	else                 return '#';
 }
 
-console.log ('\nConstructing the mandelbrot image...\n');
+console.log ('\nConstructing the mandelbrot image!\n'.green);
 
 var time0 = Date.now ();
-if (includeHTML) fs.writeFileSync(htmlFile, '<pre style="word-wrap: break-word; white-space: pre; display: block; line-height: 0.8em; letter-spacing: 1.9px;">\n');
+if (includeHTML) {
+	var css = [
+		'word-wrap: break-word;',
+		'white-space: pre;',
+		'display: block;',
+		'line-height: 0.8em;',
+		'letter-spacing: 1.9px'
+	];
 
-if (includeHTML) console.log ('    HTML file is included.');
-console.log ('    dimensions: ' + width + 'x' + height);
-console.log ('    iterations: ' + iterations + ' per pixel\n');
-console.log ('    escape mag: ' + maxMagnitude);
-console.log ('          xmin: ' + xmin);
-console.log ('          xmax: ' + xmax);
+	fs.writeFileSync(htmlFile, '<pre style="' + css.join (' ') + '">\n');
+}
+
+if (includeHTML) console.log ('    HTML file is included.'.cyan);
+console.log (('    dimensions: ' + width + 'x' + height).magenta);
+console.log (('    iterations: ' + iterations + ' per pixel\n').magenta);
+console.log (('    escape mag: ' + maxMagnitude).magenta);
+console.log (('          xmin: ' + xmin).magenta);
+console.log (('          xmax: ' + xmax).magenta);
 
 // Internal values calculated from manipulated values above
 var xWidth = Math.abs (xmin - xmax),
@@ -102,8 +149,8 @@ var xWidth = Math.abs (xmin - xmax),
 var ymin = ycenter - yHalfHeight,
 	ymax = ycenter + yHalfHeight;
 
-console.log ('          ymin: ' + ymin);
-console.log ('          ymax: ' + ymax + '\n\n');
+console.log (('          ymin: ' + ymin).magenta);
+console.log (('          ymax: ' + ymax + '\n\n').magenta);
 
 maxMagnitude *= maxMagnitude;
 
@@ -113,7 +160,7 @@ var working = ['⠋', '⠙', '⠚', '⠓'],
 	interval = 100, // milliseconds
 	tIcon0 = Date.now ();
 
-// PNG containing stream
+// PNG container stream options
 var RGBA_COLOR_TYPE = 6;
 var options = {
 	width: width,
@@ -161,19 +208,19 @@ for (var j = 0; j < height; j++) {
 		if (k == iterations) {
 			if (includeHTML) imageRow += '#';
 
-			png.data[idx]     = 0x0;  // red
-			png.data[idx + 1] = 0x0;  // green
-			png.data[idx + 2] = 0x0;  // blue
-			png.data[idx + 3] = 0xff; // alpha
+			png.data[idx]     = includeR;  // red
+			png.data[idx + 1] = includeG;  // green
+			png.data[idx + 2] = includeB;  // blue
+			png.data[idx + 3] = includeA; // alpha
 		}
 
 		else {
 			if (includeHTML) imageRow += calculatePixel (mu);
 
-			png.data[idx]     = Math.round (mu * 0xff); // red
-			png.data[idx + 1] = Math.round (mu * 0xff); // green
-			png.data[idx + 2] = Math.round (mu * 0xff); // blue
-			png.data[idx + 3] = 0xff; // alpha
+			png.data[idx]     = Math.round (mu * escapeR); // red
+			png.data[idx + 1] = Math.round (mu * escapeG); // green
+			png.data[idx + 2] = Math.round (mu * escapeB); // blue
+			png.data[idx + 3] = escapeA;                   // alpha
 		}
 
 		// Force update percentage if time to update loading icon
@@ -217,16 +264,73 @@ for (var j = 0; j < height; j++) {
 // Close the HTML tag of the ascii file if necessary
 if (includeHTML) fs.appendFileSync (htmlFile, '</pre>');
 
-// Clear the previously logged line to log the final percent and time of execution
+
+// Ensure that the animation is still running to prevent panic
+var exeCount = 0,
+	dotCount = 0,
+	dots = '';
+
+var finalAnimationInterval = setInterval(function () {
+	if (!(exeCount++ % 4)) {
+		switch (dotCount++ % 4) {
+			case 0:
+				dots = '';
+				break;
+
+			case 1:
+				dots = '.';
+				break;
+
+			case 2:
+				dots = '..';
+				break;
+
+			case 3:
+				dots = '...';
+				break;
+		}
+	}
+
+	readline.moveCursor (process.stdout, 0, -1);
+	readline.clearLine (process.stdout, 0);
+
+	console.log ('    ' + working[icon++ % working.length] + '     Flushing data to disk' + dots);
+}, interval);
+
+// Log that packing PNG data will begin
 readline.moveCursor (process.stdout, 0, -1);
 readline.clearLine (process.stdout, 0);
+console.log (('    Packing raw PNG data...').yellow);
 
+// Pipe the generated PNG information into the final output write stream
 png.pack ().pipe (fs.createWriteStream (pngFile)).on ('error', function (err) {
-	console.log ('\nThere was an error writing the PNG file');
-	console.log (err);
-});
+	console.log (('\nThere was an error writing the PNG file').red);
+	console.log (('' + err).red);
 
-console.log('    100.000% - ' + (Math.round ((Date.now () - time0)) / 1000) + 's');
-console.log ('\nThe file' + (includeHTML? 's' : '') + ' "' +
-	pngFile + '"' + (includeHTML? ' and "' + htmlFile + '"' +
-		' were' : ' was') + ' successfully saved in ' + __dirname);
+	if (includeHTML) 
+		console.log (('\nHowever, the HTML file "' + htmlFile + '" was successfully saved in ' + __dirname).yellow);
+}).on ('finish', function () {
+	// Stop the final animation from running
+	clearInterval (finalAnimationInterval);
+
+	// Clear the previously logged line to log the final percent and time of execution
+	readline.moveCursor (process.stdout, 0, -1);
+	readline.clearLine (process.stdout, 0);
+
+	var exeTimeInSeconds = (Date.now () - time0) / 1000,
+		exeTimeInMinutes = exeTimeInSeconds / 60,
+		exeTimeInHours   = exeTimeInMinutes / 24,
+
+		s = Math.round (1000 * (exeTimeInSeconds % 60)) / 1000,
+		m = Math.floor (exeTimeInSeconds / 60),
+		h = Math.floor (exeTimeInMinutes / 60),
+		d = Math.floor (exeTimeInHours   / 24);
+
+	var timeString = (d? d + 'd ' : '') + (h? h + 'h ' : '') + (m? m + 'm ' : '') + s + 's (' +
+		(1000 * exeTimeInSeconds) + 'ms)';
+
+	console.log (('    100.000% - ' + timeString).green);
+	console.log (('\nThe file' + (includeHTML? 's' : '') + ' "' +
+		pngFile + '"' + (includeHTML? ' and "' + htmlFile + '"' +
+			' were' : ' was') + ' successfully saved in ' + __dirname).green);
+});
